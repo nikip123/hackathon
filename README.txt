28a792ae629b9e039220bd1b68ab422c23568eb3
nikip123 <pardanikolina@gmail.com>
2024-12-14 04:01:27 +0100

    cjdjo

---
+++ README.txt
@@ -0,0 +1,11 @@
+Project Documentation: "Cybersecurity Open API test"
+
+Purpose of the project:
+The project was created in order to prevent web application developers from creating projects vulnerable in the field of cyberattacks.
+
+Working of the project:
+The user inputs Open API of their web application as a JSON file. The project's vulnerability is tested for following cyberattacking methods:
+1) Forceful browsing - attempt to get to forbidden resources by modyfying URL endpoints,
+2) SQL injection (including blind SQL injection) - coding SQL in user inputs, which may lead to accessing sensitive data by the hacker.
+The project returns the results in the image of percentage (number of parameters which seem to be to be divided by number of tested parameters multiplied by 100%)
+3.44 zuzia 
--- back/app.py
+++ back/app.py
@@ -1,10 +1,14 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS  # Import CORS
 from api_handler import parse_openapi
-from auth_tests import run_auth_tests
-from input_tests import run_input_tests
-from session_tests import run_session_tests
+from endpoints_testing import apiEndpoints
+import os
+#from input_tests import run_input_tests
+#from session_tests import run_session_tests
 
 app = Flask(__name__)
+CORS(app, origins=["*"], methods=["GET", "POST", "OPTIONS"], allow_headers=["Content-Type", "Authorization"])
+
 
 @app.route('/run-tests', methods=['POST'])
 def run_tests():
@@ -12,18 +16,19 @@ def run_tests():
         return jsonify({"error": "No file provided"}), 400
 
     api_spec_file = request.files['api-spec']
+    print(f"Received file: {api_spec_file.filename}")
     api_spec = parse_openapi(api_spec_file)
 
     # Run tests
-    auth_results = run_auth_tests(api_spec)
-    input_results = run_input_tests(api_spec)
-    session_results = run_session_tests()
+    end_results = apiEndpoints("https://petstore.swagger.io", api_spec)
+    #input_results = run_input_tests(api_spec)
+    #session_results = run_session_tests()
 
     # Combine results
     results = {
-        "authorization_tests": auth_results,
-        "input_validation_tests": input_results,
-        "session_tests": session_results,
+        "endpoints": end_results,
+        #"input_validation_tests": input_results,
+        #"session_tests": session_results,
     }
     return jsonify(results)
 
